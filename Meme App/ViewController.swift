e3d3c574d0983adfda2c97d494ef2895a99e4257
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-03-25 15:29:42 -0700

    Camera Button added, pickAnImageFromCamera func created, text labels added, Auto Layouts are corrected

--- Meme App/ViewController.swift
+++ Meme App/ViewController.swift
@@ -7,21 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
 //    Mark : Outlets
     @IBOutlet weak var imagePickerview: UIImageView!
-
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
+//    Mark : Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
 
   
     @IBAction func pickAnImage(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        present(pickerController, animated: true, completion: nil)
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
     }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imagePickerview.image = image
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        <#code#>
+//    }
+    
 }
 
 
