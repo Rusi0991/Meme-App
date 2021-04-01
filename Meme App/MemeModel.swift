bcacc52ea766dfe8c9fd19b1c7d65af15a7446c7
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-04-01 11:42:37 -0700

    save func,MemeModel created

---
+++ Meme App/MemeModel.swift
@@ -0,0 +1,15 @@
+//
+//  MemeModel.swift
+//  Meme App
+//
+//  Created by Ruslan Ismayilov on 4/1/21.
+//
+
+import Foundation
+import UIKit
+struct MemeModel {
+    var topText : String
+    var bottomText : String
+    var originalImage : UIImage
+    var memedImage : UIImage
+}
--- Meme App/ViewController.swift
+++ Meme App/ViewController.swift
@@ -51,11 +51,62 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    func subscribeToKeyboardNotification(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
     
+    func unsubscibeToKeyboardNotification(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc func keyboardWillShow(_ notification : Notification) {
+        if bottomTextField.isFirstResponder {
+        view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    @objc func keyboardWillHide(_ notification : Notification){
+        
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification : Notification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        topTextField.resignFirstResponder()
+        bottomTextField.resignFirstResponder()
+        return true
+    }
+    
+    func save(){
+        let meme = MemeModel(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imagePickerview.image!, memedImage: generateMemedImage())
+    }
+    
+    func generateMemedImage() -> UIImage {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return memedImage
+    }
+    
+   
+    
+}
+    
+     
+
+
 //    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
 //        <#code#>
 //    }
     
-}
+
 
 
